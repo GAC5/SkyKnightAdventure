e64c4e419da8b5b0133a891b29e892b46d4c149e
Thorn1115 <acrussi@icloud.com>
2025-04-03 13:51:03 -0400

    04/03/2025 13:50
    started bandit enemy movement towards enemy.

--- SkyKnightAdventure/Assets/Scripts/BanditEnemyScript.cs
+++ SkyKnightAdventure/Assets/Scripts/BanditEnemyScript.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField] Renderer renderer;
     [SerializeField] Animator animator;
+    [SerializeField] Rigidbody2D rigidbody;
     [SerializeField] Transform player;
     [SerializeField] int healthValue;
     [SerializeField] int attackValue;
     [SerializeField] float detectionDistance;
+    [SerializeField] float attackRange;
+    [SerializeField] float speed;
+
    
 
     // Start is called before the first frame update
@@ -24,12 +29,31 @@ public class NewBehaviourScript : MonoBehaviour
         PlayerDetect();
     }
 
+    //   transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
     private void PlayerDetect()
     {
-        if (Mathf.Abs(transform.position.x - player.position.x) <= detectionDistance)
+        float distanceToPlayer = Mathf.Abs(transform.position.x - player.position.x);
+        if (distanceToPlayer <= detectionDistance && distanceToPlayer > attackRange)
         {
-            
+            MoveTowardsPlayer();
         }
+        else if (distanceToPlayer <= detectionDistance && distanceToPlayer <= attackRange)
+        {
+            // attack code to be added here
+            Debug.Log("attacked player");
+        }
+        else
+        {
+            rigidbody.velocity = new Vector2(0, rigidbody.velocity.y);
+            // this is the section where no player is detected
+            //later, a passive animation or walk could be added to make enemies more dynamic
+        }
+    }
+
+    private void MoveTowardsPlayer()
+    {
+        Vector2 direction = (player.position - transform.position).normalized;
+        rigidbody.velocity = new Vector2(direction.x * speed, rigidbody.velocity.y);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
