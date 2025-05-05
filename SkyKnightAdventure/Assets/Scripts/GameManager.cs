b3da7d3fba36b95d6c5102b396e8cc6344452fcf
GAC5 <ga.colon05@gmail.com>
2025-05-05 14:18:52 -0400

    started "dont destroy on load" management

--- SkyKnightAdventure/Assets/Scripts/GameManager.cs
+++ SkyKnightAdventure/Assets/Scripts/GameManager.cs
@@ -7,6 +7,26 @@ using Unity.VisualScripting;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+
+
+    private void Awake()
+    {
+        //if no gamemanager / if not original gamemanager
+        if (instance != null && instance != this)
+        {
+            //destroy duplicate
+            Destroy(gameObject);
+
+            //ensures the rest of the awake method doesnt execute
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
     public void Restart()
     {
         string currentscene = SceneManager.GetActiveScene().name;
